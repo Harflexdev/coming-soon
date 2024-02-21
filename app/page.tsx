48edb65cc14ed21c5d4f530e2fe56c839006a36d
Ismaila Afeez <157530759+Harflexdev@users.noreply.github.com>
2024-02-21 12:38:55 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,11 @@ export default function Home() {
    <h1>I am Ismaila</h1>
    <p>Hello, my name is <b>Ismaila Afeez.</b></p>
    <p>I love drinking H<sub>2</sub>O and i <i>breathe </i>in O<sup>2</sup></p>
+   <p>Print this page by pressing <kbd>ctrl + p</kbd> on your keybord</p>
+   <p>Ismaila Coding in <pre><code>Html, css, javascript</code></pre></p>
+   <p>i like COD <abbr title="Call Of Duty"></abbr></p>
+   <p>i live at <address>No. 10, ayegbami street otaefun, Osogbo</address></p>
+   <p><bdo dir="ltr">left to right and can be a right to left</bdo></p>
    </>
 
   );
